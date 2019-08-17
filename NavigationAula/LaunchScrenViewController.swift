5305695646ba39a7036af0c2baf07751cffee8ce
Anthony Marques Gianeli <antony.marques@mblabs.com.br>
2019-08-17 19:37:10 -0300

    icons

--- NavigationAula/LaunchScrenViewController.swift
+++ NavigationAula/LaunchScrenViewController.swift
@@ -10,14 +10,4 @@ import UIKit
 
 class LaunchScreenViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.delay(0.4)
-    }
-    
-    func delay(_ delay:Double, closure:@escaping ()->()) {
-        let when = DispatchTime.now() + delay
-        DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
-    }
-
 }
